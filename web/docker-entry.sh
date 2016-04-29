15b8a24c9b0cb6eaf8e1ae0d16996602cbd1bd61
root <root@mo-d4c8c14d0.mo.sap.corp>
2016-04-29 14:51:08 +0000

    implement psidium changes

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo Starting Nginx
+if [ -z "$PLATFORM_PORT_80_TCP_ADDR" ]
+then
+  echo "can't find platform_port_80_tcp_addr" > /dev/stderr
+  exit 7
+fi
 sed -Ei "s/PLATFORM_ADDR/$PLATFORM_PORT_80_TCP_ADDR/" /etc/nginx/sites-available/mattermost
 sed -Ei "s/PLATFORM_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost
 nginx -g 'daemon off;'
